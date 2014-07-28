e6b99acdefe39112a0357a03e3bccef4fbb0fe19
Rafał Słota <roxeon@gmail.com>
2014-07-28 11:48:04 +0200

    VFS-754: add ensure_list

--- src/vcn_utils.erl
+++ src/vcn_utils.erl
@@ -11,7 +11,7 @@
 -module(vcn_utils). %todo remove this module from veilcluster
 
 %% API
--export([ensure_running/1, pmap/2, pforeach/2, time/0, mtime/0, record_type/1, ensure_binary/1]).
+-export([ensure_running/1, pmap/2, pforeach/2, time/0, mtime/0, record_type/1, ensure_binary/1, ensure_list/1]).
 
 %% ====================================================================
 %% API functions
@@ -137,5 +137,13 @@ ensure_binary(Bin) when is_binary(Bin) ->
     Bin;
 ensure_binary(List) when is_list(List) ->
     list_to_binary(List);
-ensure_binary(Term) ->
-    term_to_binary(Term).
+ensure_binary(Integer) when is_integer(Integer) ->
+    integer_to_binary(Integer).
+
+
+ensure_list(Binary) when is_binary(Binary) ->
+    binary_to_list(Binary);
+ensure_list(Integer) when is_integer(Integer) ->
+    integer_to_list(Integer);
+ensure_list(List) when is_list(List) ->
+    List.
