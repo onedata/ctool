ecfaa041d09ad4b59db4bfc1a7719d30d3adf63a
Krzysztof Trzepla <krzysztof.trzepla@gmail.com>
2015-02-11 14:27:34 +0100

    VFS-997 Add init macro.

--- include/test/test_utils.hrl
+++ include/test/test_utils.hrl
@@ -18,9 +18,18 @@
 %% Returns absolute path to given file in the test data directory
 -define(TEST_FILE(Config, X), filename:join(?config(data_dir, Config), X)).
 
+%% Catches suite init exceptions
+-define(TRY_INIT(Config, EnvDescription),
+    try
+        test_node_starter:prepare_test_environment(Config, EnvDescription, ?MODULE)
+    catch
+        A:B -> ct:print("~p:~p~n~p", [A, B, erlang:get_stacktrace()])
+    end
+).
+
 %% Utility macros
 -define(CURRENT_HOST, list_to_atom(lists:last(string:tokens(atom_to_list(node()), "@")))).
--define(NODE(NodeHost, NodeName), list_to_atom(atom_to_list(NodeName)++"@"++atom_to_list(NodeHost))).
+-define(NODE(NodeHost, NodeName), list_to_atom(atom_to_list(NodeName) ++ "@" ++ atom_to_list(NodeHost))).
 -define(GET_NODE_NAME(FullName), list_to_atom(hd(string:tokens(atom_to_list(FullName), "@")))).
 -define(GET_HOST(FullName), list_to_atom(lists:last(string:tokens(atom_to_list(FullName), "@")))).
 
