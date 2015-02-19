eb4a2e701b657c7579937ae03efffde77d5ca91a
wrzeszcz <wrzeszcz@agh.edu.pl>
2015-02-19 15:56:24 +0100

    VFS-1000, merge

--- src/test/test_node_starter.erl
+++ src/test/test_node_starter.erl
@@ -25,40 +25,47 @@
 %% @end
 %%--------------------------------------------------------------------
 -spec prepare_test_environment(Config :: list(), DescriptionFile :: string(),
-    Module :: module()) -> Result :: list().
+    Module :: module()) -> Result :: list() | {fail, tuple()}.
 prepare_test_environment(Config, DescriptionFile, Module) ->
-    DataDir = proplists:get_value(data_dir, Config),
-    CtTestRoot = filename:join(DataDir, ".."),
-    ProjectRoot = filename:join(CtTestRoot, ".."),
+    try
+        DataDir = proplists:get_value(data_dir, Config),
+        CtTestRoot = filename:join(DataDir, ".."),
+        ProjectRoot = filename:join(CtTestRoot, ".."),
 
-    ConfigWithPaths =
-        [{ct_test_root, CtTestRoot}, {project_root, ProjectRoot} | Config],
+        ConfigWithPaths =
+            [{ct_test_root, CtTestRoot}, {project_root, ProjectRoot} | Config],
 
-    ProviderUpScript =
-        filename:join([ProjectRoot, "bamboos", "docker", "provider_up.py"]),
+        ProviderUpScript =
+            filename:join([ProjectRoot, "bamboos", "docker", "provider_up.py"]),
 
-    StartLog = cmd([ProviderUpScript, "-b", ProjectRoot, DescriptionFile]),
-    EnvDesc = json_parser:parse_json_binary_to_atom_proplist(StartLog),
+        StartLog = cmd([ProviderUpScript, "-b", ProjectRoot, DescriptionFile]),
+        EnvDesc = json_parser:parse_json_binary_to_atom_proplist(StartLog),
 
-    Dns = ?config(dns, EnvDesc),
-    Workers = ?config(op_worker_nodes, EnvDesc),
-    CCMs = ?config(op_ccm_nodes, EnvDesc),
+        Dns = ?config(dns, EnvDesc),
+        Workers = ?config(op_worker_nodes, EnvDesc),
+        CCMs = ?config(op_ccm_nodes, EnvDesc),
 
-    erlang:set_cookie(node(), oneprovider_node),
-    os:cmd("echo nameserver " ++ atom_to_list(Dns) ++ " > /etc/resolv.conf"),
+        erlang:set_cookie(node(), oneprovider_node),
+        os:cmd("echo nameserver " ++ atom_to_list(Dns) ++ " > /etc/resolv.conf"),
 
-    ping_nodes(lists:append(CCMs, Workers)),
+        ping_nodes(lists:append(CCMs, Workers)),
 
-    cluster_state_notifier:cast({subscribe_for_init, self(), length(Workers)}),
-    receive
-        init_finished -> ok
-    after
-        timer:seconds(50) -> throw(timeout)
-    end,
+        cluster_state_notifier:cast({subscribe_for_init, self(), length(Workers)}),
+        receive
+            init_finished -> ok
+        after
+            timer:seconds(50) -> throw(timeout)
+        end,
 
-    ok = load_modules(CCMs ++ Workers, [Module]),
+        ok = load_modules(CCMs ++ Workers, [Module]),
 
-    lists:append(ConfigWithPaths, proplists:delete(dns, EnvDesc)).
+        lists:append(ConfigWithPaths, proplists:delete(dns, EnvDesc))
+    catch
+        E1:E2 ->
+            ct:print("Prepare of environment failed ~p:~p~n~p", [E1, E2, erlang:get_stacktrace()]),
+            clean_environment(Config),
+            {fail, {init_failed, E1, E2}}
+    end.
 
 %%--------------------------------------------------------------------
 %% @doc
@@ -95,7 +102,7 @@ clean_environment(Config) ->
 %%--------------------------------------------------------------------
 -spec ping_nodes(Nodes :: list()) -> ok | no_return().
 ping_nodes(Nodes) ->
-    ping_nodes(Nodes, 30).
+    ping_nodes(Nodes, 300).
 ping_nodes(_Nodes, 0) ->
     throw(nodes_connection_error);
 ping_nodes(Nodes, Tries) ->
