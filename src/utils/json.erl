1f3ae11e6dc2ba0f27ba8685c658a6cbbc7c83f4
Tomasz Lichon <xorver@gmail.com>
2015-10-29 12:47:04 +0100

    VFS-1291 Add json utility functions.

---
+++ src/utils/json.erl
@@ -0,0 +1,48 @@
+%%%--------------------------------------------------------------------
+%%% @author Tomasz Lichon
+%%% @copyright (C) 2015 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%--------------------------------------------------------------------
+%%% @doc
+%%% Json parser utility functions
+%%% @end
+%%%--------------------------------------------------------------------
+-module(json).
+-author("Tomasz Lichon").
+
+%% API
+-export([encode/1, decode/1]).
+
+%%%===================================================================
+%%% API
+%%%===================================================================
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Convenience function that convert an erlang term to JSON, producing
+%% binary result. The output is in UTF8 encoding.
+%% Possible terms, can be nested:
+%% {struct, Props} - Props is a structure as a proplist, e.g.: [{id, 13}, {message, "mess"}]
+%% {Props} - alias for above
+%% {array, Array} - Array is a list, e.g.: [13, "mess"]
+%% @end
+%%--------------------------------------------------------------------
+-spec encode(term()) -> binary().
+encode(Term) ->
+    Encoder = mochijson2:encoder([{utf8, true}]),
+    iolist_to_binary(Encoder(Term)).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Convenience function that convert JSON binary to an erlang term.
+%% @end
+%%--------------------------------------------------------------------
+-spec decode(binary()) -> term().
+decode(JSON) ->
+    try mochijson2:decode(JSON, [{format, proplist}]) catch _:_ -> throw(invalid_json) end.
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
