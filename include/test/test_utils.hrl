7b1981e3add558062f28aea43aaf0a887f834703
Tomasz Lichon <xorver@gmail.com>
2016-02-08 10:25:16 +0100

    VFS-1421 Add GET_DOMAIN macro.

--- include/test/test_utils.hrl
+++ include/test/test_utils.hrl
@@ -32,5 +32,6 @@
 -define(NODE(NodeHost, NodeName), list_to_atom(atom_to_list(NodeName) ++ "@" ++ atom_to_list(NodeHost))).
 -define(GET_NODE_NAME(FullName), list_to_atom(hd(string:tokens(atom_to_list(FullName), "@")))).
 -define(GET_HOSTNAME(FullName), list_to_atom(lists:last(string:tokens(atom_to_list(FullName), "@")))).
+-define(GET_DOMAIN(FullName), list_to_atom(string:join(lists:sublist(string:tokens(atom_to_list(?GET_HOSTNAME(FullName)), "."), 2, 10), "."))).
 
 -endif.
