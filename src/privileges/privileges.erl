5db6a083b483714c31af95a96a01e5d20b726b69
Łukasz Opioła <lukasz.opiola@gmail.com>
2016-08-30 13:14:28 +0200

    VFS-2405 Add privileges for shares management

--- src/privileges/privileges.erl
+++ src/privileges/privileges.erl
@@ -20,7 +20,7 @@
 -type space_privilege() :: space_invite_user | space_remove_user |
 space_invite_group | space_remove_group | space_set_privileges |
 space_remove | space_add_provider | space_remove_provider |
-space_change_data | space_view_data.
+space_change_data | space_view_data | space_manage_shares.
 
 %% User privileges with regards to group management.
 -type group_privilege() :: group_change_data | group_invite_user |
