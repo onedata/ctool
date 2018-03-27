8f1305bcf6d06fbeee18937623ede367f983511f
Łukasz Opioła <lukasz.opiola@gmail.com>
2018-03-27 11:57:17 +0200

    Squashed 'bamboos/' changes from 9333ecfc..4d706bb8
    4d706bb8 VFS-4267 Freeze default worker and builder docker at certain versions
    4ccc09da VFS-4267 Adjust example app description to the new appmock
    41e10dd5 Merge pull request #217 in VFS/bamboos from feature/VFS-4168-update-acceptance-gui-tests-after to develop
    1798784c Merge pull request #216 in VFS/bamboos from feature/VFS-4197-prevent-removing-k8s-containers to develop
    d65eaa74 VFS-4083 Added flag to not mount /etc/passwd
    f26f3716 VFS-4168 Added ip in script output
    a15a1e30 VFS-4197 Added detecting k8s containers
    b4d19509 Merge pull request #215 in VFS/bamboos from feature/VFS-4166-add-removing-all-dockers-on-begining to develop
    71715d41 VFS-4166 bugfix
    70c76959 VFS-4166 improve logging
    179c5422 VFS-4166 bugfix and refactor
    9bc3c206 Merge pull request #212 in VFS/bamboos from feature/VFS-4054-do-not-store-ips-in-cluster-manager to develop
    c1f2a823 VFs-4054 Store node ip in cluster_worker app config
    979733ff Merge pull request #214 in VFS/bamboos from feature/VFS-4166-add-removing-all-dockers-on-begining to develop
    2fd74e56 Merge pull request #213 in VFS/bamboos from feature/VFS-4178-add-flat-storage-path-mapping-option to develop
    184f97c7 VFS-4166 add removin all docker containers on the start of ct_run and test_run
    39c78fa3 VFS-4036 Added storage path type storage option
    822b65c2 Merge pull request #211 in VFS/bamboos from feature/VFS-3978-test-ca-should-not-be-included-in to develop
    284b5eea VFS-3978 Override envs related to test certs when starting onepanel
    aaf1d64f Merge pull request #209 in VFS/bamboos from feature/VFS-4028-update-gui-acceptance-tests-to-lets to develop
    c2e131b4 Merge pull request #210 in VFS/bamboos from feature/VFS-3978-test-ca-should-not-be-included-in to develop
    97d852e8 VFS-4028 Added new entry to /etc/hosts with write-to-etc-hosts enabled
    9a85f26b VFS-3978 Add generation of trusted certs with a predefined CA during env setup   * change docker domains to .test instead of .dev
    e1a54b17 Merge pull request #208 in VFS/bamboos from feature/VFS-3837-implement-null-storage-helper-for to develop
    d2b70f13 VFS-3873 Added null device storage helper
    aaa6efb2 Merge pull request #207 in VFS/bamboos from feature/VFS-2501-remove-location-service-and-identity to develop
    
    git-subtree-dir: bamboos
    git-subtree-split: 4d706bb87eb9ddd16b731a73d4bda96bac41afbb

--- docker/test_run.py
+++ docker/test_run.py
@@ -12,8 +12,9 @@ import argparse
 import os
 import platform
 import sys
+import time
 
-from environment.common import HOST_STORAGE_PATH
+from environment.common import HOST_STORAGE_PATH, remove_dockers_and_volumes
 from environment import docker
 import glob
 import xml.etree.ElementTree as ElementTree
@@ -55,7 +56,7 @@ parser = argparse.ArgumentParser(
 parser.add_argument(
     '--image', '-i',
     action='store',
-    default='onedata/worker',
+    default='onedata/worker:v57',
     help='Docker image to use as a test master.',
     dest='image')
 
@@ -100,7 +101,14 @@ parser.add_argument(
     action='store',
     help="Name of docker container where tests will be running",
     dest='docker_name',
-    default='test_run_docker'
+    default='test_run_docker_{}'.format(int(time.time()))
+)    
+
+parser.add_argument(
+    '--no-etc-passwd',
+    action='store_true',
+    help="Do not mount /etc/passwd to container, warning: some tests may not work",
+    dest='no_etc_passwd'
 )    
 
 [args, pass_args] = parser.parse_known_args()
@@ -155,15 +163,21 @@ command = command.format(
 # 128MB or more required for chrome tests to run with xvfb
 run_params = ['--shm-size=128m']
 
+remove_dockers_and_volumes()
+
+reflect=[(script_dir, 'rw'),
+         ('/var/run/docker.sock', 'rw'),
+         (HOST_STORAGE_PATH, 'rw')]
+
+if not args.no_etc_passwd:
+    reflect.extend([('/etc/passwd', 'ro')])
+         
 ret = docker.run(tty=True,
                  rm=True,
                  interactive=True,
                  name=args.docker_name,
                  workdir=script_dir,
-                 reflect=[(script_dir, 'rw'),
-                          ('/var/run/docker.sock', 'rw'),
-                          ('/etc/passwd', 'ro'),
-                          (HOST_STORAGE_PATH, 'rw')],
+                 reflect = reflect,
                  volumes=[(os.path.join(os.path.expanduser('~'),
                                         '.docker'), '/tmp/.docker', 'rw')],
                  image=args.image,
