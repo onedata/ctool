0b86fe57e63b896bf109ed57d16105947b2d2ca8
Wojciech Geisler <wojciech.geisler@gmail.com>
2019-10-09 17:55:24 +0200

    VFS-5731 Add macros for headers used by gui app

--- include/http/headers.hrl
+++ include/http/headers.hrl
@@ -24,7 +24,21 @@
 % Header carrying original peer IP for request proxied by http_port_forwarder
 -define(HTTP_X_ONEDATA_FORWARDED_FOR, <<"x-onedata-forwarded-for">>).
 
-% Standard HTTP headers
+%% Standard HTTP headers
+
 -define(HTTP_CONTENT_TYPE, <<"content-type">>).
+
+% Header carrying allowed HTTP methods
+-define(HTTP_ALLOW, <<"allow">>).
+
+-define(HTTP_ACCESS_CONTROL_ALLOW_ORIGIN, <<"access-control-allow-origin">>).
+-define(HTTP_X_FRAME_OPTIONS, <<"x-frame-options">>).
+
+-define(HTTP_ACCESS_CONTROL_ALLOW_ORIGIN, <<"access-control-allow-origin">>).
+-define(HTTP_ACCESS_CONTROL_ALLOW_METHODS, <<"access-control-allow-methods">>).
+-define(HTTP_ACCESS_CONTROL_ALLOW_HEADERS, <<"access-control-allow-headers">>).
+-define(HTTP_ACCESS_CONTROL_REQUEST_METHOD, <<"access-control-request-method">>).
+-define(HTTP_LOCATION, <<"location">>).
+-define(HTTP_COOKIE, <<"cookie">>).
 
 -endif.
