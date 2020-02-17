80aa55587e1edd9f30fdf191fe5f251ee712cc20
wrzeszcz <michalwrzeszcz@gmail.com>
2020-02-17 23:29:45 +0100

    VFS-5983 Extend consistent hashing to support HA

--- src/hashing/consistent_hashing.erl
+++ src/hashing/consistent_hashing.erl
@@ -13,14 +13,18 @@
 -module(consistent_hashing).
 -author("Tomasz Lichon").
 
--type chash() :: chash:chash() | node().
+-record(ring, {
+    type :: single_node | multi_node | broken,
+    chash :: chash:chash() | node(),
+    broken_nodes = [] :: [node()]
+}).
+
+-type ring() :: #ring{}.
 
 %% API
 -export([init/1, cleanup/0, get_chash_ring/0, set_chash_ring/1,
-    get_node/1, get_nodes/2, get_all_nodes/0]).
-
--define(SINGLE_NODE_CHASH(Node), Node).
--define(IS_SINGLE_NODE_CHASH(CHash), is_atom(CHash)).
+    set_broken_node/1, set_fixed_node/1,
+    get_node/1, get_nodes/2, get_alive_and_broken_nodes/2, get_all_nodes/0]).
 
 %%%===================================================================
 %%% API
@@ -33,7 +37,7 @@
 %%--------------------------------------------------------------------
 -spec init([node()]) -> ok.
 init([Node]) ->
-    set_chash_ring(?SINGLE_NODE_CHASH(Node));
+    set_chash_ring(#ring{type = single_node, chash = Node});
 init(Nodes) ->
     case get_chash_ring() of
         undefined ->
@@ -42,7 +46,8 @@ init(Nodes) ->
             CHash = lists:foldl(fun({I, Node}, CHashAcc) ->
                 chash:update(get_nth_index(I, CHashAcc), Node, CHashAcc)
             end, InitialCHash, lists:zip(lists:seq(1, length(Nodes)), Nodes)),
-            set_chash_ring(CHash);
+            Ring = #ring{type = multi_node, chash = CHash},
+            set_chash_ring(Ring);
         _ ->
             ok
     end.
@@ -61,7 +66,7 @@ cleanup() ->
 %% Returns consistent hashing ring.
 %% @end
 %%--------------------------------------------------------------------
--spec get_chash_ring() -> chash() | undefined.
+-spec get_chash_ring() -> ring() | undefined.
 get_chash_ring() ->
     ctool:get_env(chash, undefined).
 
@@ -70,13 +75,40 @@ get_chash_ring() ->
 %% Sets consistent hashing ring.
 %% @end
 %%--------------------------------------------------------------------
--spec set_chash_ring(chash()) -> ok.
+-spec set_chash_ring(ring()) -> ok.
 set_chash_ring(CHash) ->
     ctool:set_env(chash, CHash).
 
 %%--------------------------------------------------------------------
 %% @doc
+%% Sets information about broken node.
+%% @end
+%%--------------------------------------------------------------------
+-spec set_broken_node(node()) -> ok.
+set_broken_node(BrokenNode) ->
+    #ring{broken_nodes = BrokenNodes} = Ring = get_chash_ring(),
+    Ring2 = Ring#ring{type = broken,
+        broken_nodes = lists:usort([BrokenNode | BrokenNodes -- [BrokenNode]])},
+    ctool:set_env(chash, Ring2).
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Sets information about fixed node.
+%% @end
+%%--------------------------------------------------------------------
+-spec set_fixed_node(node()) -> ok.
+set_fixed_node(FixedNodes) ->
+    #ring{broken_nodes = BrokenNodes} = Ring = get_chash_ring(),
+    Ring2 = case BrokenNodes -- [FixedNodes] of
+        [] -> Ring#ring{type = multi_node, broken_nodes = []};
+        BrokenNodes2 -> Ring#ring{broken_nodes = BrokenNodes2}
+    end,
+    ctool:set_env(chash, Ring2).
+
+%%--------------------------------------------------------------------
+%% @doc
 %% Get node that is responsible for the data labeled with given term.
+%% Throws error if node is broken.
 %% @end
 %%--------------------------------------------------------------------
 -spec get_node(term()) -> node().
@@ -84,12 +116,19 @@ get_node(Label) ->
     case get_chash_ring() of
         undefined ->
             error(chash_ring_not_initialized);
-        Node when ?IS_SINGLE_NODE_CHASH(Node) ->
+        #ring{type = single_node, chash = Node} ->
             Node;
-        CHash ->
+        #ring{type = multi_node, chash = CHash} ->
             Index = chash:key_of(Label),
             [{_, BestNode}] = chash:successors(Index, CHash, 1),
-            BestNode
+            BestNode;
+        #ring{type = broken, chash = CHash, broken_nodes = BrokenNodes} ->
+            Index = chash:key_of(Label),
+            [{_, BestNode}] = chash:successors(Index, CHash, 1),
+            case lists:member(BestNode, BrokenNodes) of
+                true -> throw(broken_node);
+                false -> BestNode
+            end
     end.
 
 %%--------------------------------------------------------------------
@@ -102,11 +141,42 @@ get_nodes(Label, NodesNum) ->
     case get_chash_ring() of
         undefined ->
             error(chash_ring_not_initialized);
-        Node when ?IS_SINGLE_NODE_CHASH(Node) ->
+        #ring{type = single_node, chash = Node} ->
             [Node];
-        CHash ->
+        #ring{type = multi_node, chash = CHash} ->
             Index = chash:key_of(Label),
-            lists:map(fun({_, Node}) -> Node end, chash:successors(Index, CHash, NodesNum))
+            lists:map(fun({_, Node}) -> Node end, chash:successors(Index, CHash, NodesNum));
+        #ring{type = broken, chash = CHash, broken_nodes = BrokenNodes} ->
+            Index = chash:key_of(Label),
+            lists:map(fun({_, Node}) ->
+                case lists:member(Node, BrokenNodes) of
+                    true -> throw(broken_node);
+                    false -> Node
+                end
+            end, chash:successors(Index, CHash, NodesNum))
+    end.
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Get nodes that are responsible for the data labeled with given term.
+%% Returns two lists: alvie and broken nodes.
+%% @end
+%%--------------------------------------------------------------------
+-spec get_alive_and_broken_nodes(term(), non_neg_integer()) -> {AliveNodes :: [node()], BrokenNodes :: [node()]}.
+get_alive_and_broken_nodes(Label, NodesNum) ->
+    case get_chash_ring() of
+        undefined ->
+            error(chash_ring_not_initialized);
+        #ring{type = single_node, chash = Node} ->
+            {[Node], []};
+        #ring{type = multi_node, chash = CHash} ->
+            Index = chash:key_of(Label),
+            Nodes = lists:map(fun({_, Node}) -> Node end, chash:successors(Index, CHash, NodesNum)),
+            {Nodes, []};
+        #ring{type = broken, chash = CHash, broken_nodes = BrokenNodes} ->
+            Index = chash:key_of(Label),
+            Nodes = chash:successors(Index, CHash, NodesNum),
+            filter_nodes(Nodes, BrokenNodes)
     end.
 
 %%--------------------------------------------------------------------
@@ -119,9 +189,9 @@ get_all_nodes() ->
     case get_chash_ring() of
         undefined ->
             error(chash_ring_not_initialized);
-        Node when ?IS_SINGLE_NODE_CHASH(Node) ->
+        #ring{type = single_node, chash = Node} ->
             [Node];
-        CHash ->
+        #ring{chash = CHash} ->
             NodesWithIndices = chash:nodes(CHash),
             {_, Nodes} = lists:unzip(NodesWithIndices),
             lists:usort(Nodes)
@@ -140,3 +210,16 @@ get_all_nodes() ->
 get_nth_index(N, CHash) ->
     {Index, _} = lists:nth(N, chash:nodes(CHash)),
     Index.
+
+-spec filter_nodes(ResponsibleNodes :: [node()], AllBrokenNodes :: [node()]) ->
+    {GoodNodes :: [node()], BrokenNodes :: [node()]}.
+filter_nodes([], _BrokenNodes) ->
+    {[], []};
+filter_nodes([{_, Node} | Nodes], BrokenNodes) ->
+    {AliveNodesAns, BrokenNodesAns} = filter_nodes(Nodes, BrokenNodes),
+    case lists:member(Node, BrokenNodes) of
+        true ->
+            {AliveNodesAns, [Node | BrokenNodesAns]};
+        _ ->
+            {[Node | AliveNodesAns], BrokenNodesAns}
+    end.
