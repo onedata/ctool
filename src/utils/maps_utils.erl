89232b238a8f0bc80dd988520bbc2cb43ee3ecd6
pduleba <duleba.pio@gmail.com>
2020-09-08 10:58:40 +0200

    VFS-6661 rename function

--- src/utils/maps_utils.erl
+++ src/utils/maps_utils.erl
@@ -15,7 +15,7 @@
 %% API
 -export([merge/1]).
 -export([remove_undefined/1, undefined_to_null/1]).
--export([is_component/2]).
+-export([is_submap/2]).
 
 %%%===================================================================
 %%% API functions
@@ -65,7 +65,6 @@ merge(ListOfMaps) ->
 %% Checks if all associations from the first map exists in the second one.
 %% @end
 %%-------------------------------------------------------------------
--spec is_component
-    (#{Key => Value}, #{Key => Value}) -> boolean().
-is_component(MapA, MapB) ->
-    MapA =:= maps:with(maps:keys(MapA), MapB).
+-spec is_submap(#{Key => Value}, #{Key => Value}) -> boolean().
+is_submap(Smaller, Bigger) ->
+    Smaller =:= maps:with(maps:keys(Smaller), Bigger).
