89bcde8ef331bd589facd77bd001595d1169e9c3
Michał Stanisz <mch.stanisz@gmail.com>
2021-03-16 09:57:16 +0100

    VFS-7351 Improvements according to code review

--- src/utils/tar_utils.erl
+++ src/utils/tar_utils.erl
@@ -13,10 +13,21 @@
 -author("Michal Stanisz").
 
 %% API
--export([prepare_header/5, data_padding/1, eof/0]).
+-export([prepare_header/5, data_padding/1, ending_marker/0]).
 
 -type tar_header() :: binary().
 -export_type([tar_header/0]).
+
+-define(BLOCK_SIZE, 512).
+-define(DIRECTORY_TYPE_FLAG, <<"5">>).
+-define(FILE_TYPE_FLAG, <<"0">>).
+-define(LONG_LINK_TYPE_FLAG, <<"L">>).
+-define(NUL, <<0>>).
+-define(VALUE_PAD, <<"0">>).
+-define(LONG_LINK_HEADER, <<46, 47, 46, 47, "@LongLink">>).
+-define(MAX_FILENAME_LENGTH, 100).
+-define(CHECKSUM_PLACEHOLDER, binary:copy(<<32>>, 8)).
+-define(CHECKSUM_ENDING, <<0, 32>>).
 
 %%%===================================================================
 %%% API
@@ -27,23 +38,23 @@
 prepare_header(Filename, FileSize, Mode, Timestamp, IsDir) ->
     {Type, FinalFilename} = case IsDir of
         true ->
-            {<<"5">>, <<Filename/binary, "/">>};
+            {?DIRECTORY_TYPE_FLAG, <<Filename/binary, "/">>};
         false ->
-            {<<"0">>, Filename}
+            {?FILE_TYPE_FLAG, Filename}
     end,
     prepare_header(FinalFilename, byte_size(Filename), FileSize, Mode, Timestamp, Type).
 
 
 -spec data_padding(non_neg_integer()) -> binary().
-data_padding(FileSize) when FileSize rem 512 == 0 -> <<>>;
+data_padding(FileSize) when FileSize rem ?BLOCK_SIZE == 0 -> <<>>;
 data_padding(FileSize) ->
-    BytesToFill = (512 - FileSize rem 512),
-    str_utils:pad_right(<<>>, BytesToFill, <<0>>).
+    BytesToFill = (?BLOCK_SIZE - FileSize rem ?BLOCK_SIZE),
+    binary:copy(?NUL, BytesToFill).
 
 
--spec eof() -> binary().
-eof() ->
-    str_utils:pad_right(<<>>, 1024, <<0>>).
+-spec ending_marker() -> binary().
+ending_marker() ->
+    binary:copy(?NUL, 2 * ?BLOCK_SIZE).
 
 
 %%%===================================================================
@@ -52,12 +63,13 @@ eof() ->
 
 %% @private
 -spec prepare_header(binary(), FilenameSize :: non_neg_integer(), FileSize :: non_neg_integer(), 
-    Mode :: non_neg_integer(), Timestamp :: non_neg_integer(), boolean()) -> tar_header().
-prepare_header(Filename, FilenameSize, FileSize, Mode, Timestamp, Type) when FilenameSize > 100 ->
+    Mode :: non_neg_integer(), Timestamp :: non_neg_integer(), binary()) -> tar_header().
+prepare_header(Filename, FilenameSize, FileSize, Mode, Timestamp, Type) when FilenameSize > ?MAX_FILENAME_LENGTH ->
     LongLinkHeader = prepare_header(
-        <<46, 47, 46, 47, "@LongLink">>, 13, FilenameSize, 0, 0, <<"L">>),
+        ?LONG_LINK_HEADER, byte_size(?LONG_LINK_HEADER), FilenameSize, 0, 0, ?LONG_LINK_TYPE_FLAG),
+    TrimmedFilename = binary:part(Filename, 0, ?MAX_FILENAME_LENGTH),
     FileHeader = prepare_header(
-        binary:part(Filename, 0, 100), min(byte_size(Filename), 100), FileSize, Mode, Timestamp, Type),
+        TrimmedFilename, byte_size(TrimmedFilename), FileSize, Mode, Timestamp, Type),
     <<
         LongLinkHeader/binary,
         Filename/binary,
@@ -65,28 +77,28 @@ prepare_header(Filename, FilenameSize, FileSize, Mode, Timestamp, Type) when Fil
         FileHeader/binary
     >>;
 
-prepare_header(Filename, FilenameSize, FileSize, Mode, Timestamp, Type) when FilenameSize =< 100 ->
+prepare_header(Filename, FilenameSize, FileSize, Mode, Timestamp, Type) when FilenameSize =< ?MAX_FILENAME_LENGTH ->
     % use root(0) as owner/group - by default it will be overwritten when untarring
-    PaddedOwner = str_utils:pad_left(format_octal(0), 7, <<"0">>),
-    PaddedGroup = str_utils:pad_left(format_octal(0), 7, <<"0">>),
-    PaddedSize = str_utils:pad_left(format_octal(FileSize), 11, <<"0">>),
-    PaddedMode = str_utils:pad_left(format_octal(Mode), 7, <<"0">>),
-    PaddedTimestamp = str_utils:pad_left(format_octal(Timestamp), 11, <<"0">>),
+    PaddedOwner = str_utils:pad_left(format_octal(0), 7, ?VALUE_PAD),
+    PaddedGroup = str_utils:pad_left(format_octal(0), 7, ?VALUE_PAD),
+    PaddedSize = str_utils:pad_left(format_octal(FileSize), 11, ?VALUE_PAD),
+    PaddedMode = str_utils:pad_left(format_octal(Mode), 7, ?VALUE_PAD),
+    PaddedTimestamp = str_utils:pad_left(format_octal(Timestamp), 11, ?VALUE_PAD),
     HeaderFirstPart = <<
-        (str_utils:pad_right(Filename, 100, <<0>>))/binary,
+        (str_utils:pad_right(Filename, ?MAX_FILENAME_LENGTH, ?NUL))/binary,
         PaddedMode/binary, 0,
         PaddedOwner/binary, 0,
         PaddedGroup/binary, 0,
         PaddedSize/binary, 0,
         PaddedTimestamp/binary, 0
     >>,
-    HeaderSecondPart = <<(str_utils:pad_right(<<Type/binary >>, 101, <<0>>))/binary>>,
-    ChecksumPlaceholder = binary:copy(<<32>>, 8),
+    HeaderSecondPart = <<(str_utils:pad_right(<<Type/binary>>, 101, ?NUL))/binary>>,
     Checksum = calculate_checksum(
-        <<HeaderFirstPart/binary, ChecksumPlaceholder/binary, HeaderSecondPart/binary>>),
-    PaddedChecksum = <<(str_utils:pad_left(format_octal(Checksum), 6, <<"0">>))/binary, 0, 32>>,
+        <<HeaderFirstPart/binary, (?CHECKSUM_PLACEHOLDER)/binary, HeaderSecondPart/binary>>),
+    PaddedChecksum = 
+        <<(str_utils:pad_left(format_octal(Checksum), 6, ?VALUE_PAD))/binary, (?CHECKSUM_ENDING)/binary>>,
     str_utils:pad_right(
-        <<HeaderFirstPart/binary, PaddedChecksum/binary, HeaderSecondPart/binary>>, 512, <<0>>).
+        <<HeaderFirstPart/binary, PaddedChecksum/binary, HeaderSecondPart/binary>>, ?BLOCK_SIZE, ?NUL).
 
 
 %% @private
