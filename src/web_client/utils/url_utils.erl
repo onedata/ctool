ad20625521fd477e9adfaeaa29e3758eef2d6c4d
duleba.pio <duleba.pio@gmail.com>
2021-05-20 21:11:57 +0200

    VFS-7667 add parse_secure function to url_utils module

--- src/web_client/utils/url_utils.erl
+++ src/web_client/utils/url_utils.erl
@@ -11,10 +11,17 @@
 -module(url_utils).
 -author("Lukasz Opiola").
 
+-include("errors.hrl").
 -include_lib("hackney/include/hackney_lib.hrl").
 
 %% API
--export([parse/1]).
+-export([
+    parse/1,
+    infer_components/1
+]).
+
+-define(DEFAULT_HTTP_PORT, 80).
+-define(DEFAULT_HTTPS_PORT, 443).
 
 %%%===================================================================
 %%% API
@@ -38,3 +45,81 @@ parse(Uri) ->
         path => Path,
         qs => QueryString
     }.
+
+
+%%--------------------------------------------------------------------
+%% @doc
+%% Parses given URI and returns identified parts.
+%% Function infers missing schema or port in incomplete urls if possible.
+%% @end
+%%--------------------------------------------------------------------
+-spec infer_components(http_client:url()) -> #{scheme => http | https,
+host => binary(), port => non_neg_integer(), path => binary(), qs => binary()}.
+infer_components(URL) ->
+    try
+        infer_components_insecure(URL)
+    catch
+        _:_ -> throw(?ERROR_MALFORMED_DATA)
+    end.
+
+
+%%%===================================================================
+%%% Helpers
+%%%===================================================================
+
+
+%% @private
+-spec infer_components_insecure(http_client:url()) -> #{scheme => http | https,
+host => binary(), port => non_neg_integer(), path => binary(), qs => binary()}.
+infer_components_insecure(URL) ->
+    URLTrimmed = list_to_binary(string:trim(binary_to_list(URL))),
+    #{
+        host := ParsedHost, path := ParsedPath, qs := ParsedQueryString
+    } = parse(URLTrimmed),
+    URLScheme = get_scheme_from_url(URLTrimmed),
+    URLPort = get_port_from_url(URLTrimmed, ParsedHost),
+
+    {FinalScheme, FinalPort} = case {URLScheme, URLPort} of
+        {undefined, undefined} ->
+            throw(?ERROR_MALFORMED_DATA);
+        {undefined, ?DEFAULT_HTTPS_PORT} ->
+            {https, ?DEFAULT_HTTPS_PORT};
+        {undefined, ?DEFAULT_HTTP_PORT} ->
+            {http, ?DEFAULT_HTTP_PORT};
+        %% TODO: VFS-7682 - following match should throw an error
+        {undefined, CustomPort} ->
+            {http, CustomPort};
+        {http, CustomPort} ->
+            {http, utils:ensure_defined(CustomPort, ?DEFAULT_HTTP_PORT)};
+        {https, CustomPort} ->
+            {https, utils:ensure_defined(CustomPort, ?DEFAULT_HTTPS_PORT)}
+    end,
+
+    #{
+        scheme => FinalScheme,
+        host => ParsedHost,
+        port => FinalPort,
+        path => ParsedPath,
+        qs => ParsedQueryString
+    }.
+
+
+%% @private
+-spec get_scheme_from_url(http_client:url()) -> http | https | undefined.
+get_scheme_from_url(URL) ->
+    case hd(binary:split(URL, [<<"://">>])) of
+        <<"https">> -> https;
+        <<"http">> -> http;
+        _ -> undefined
+    end.
+
+
+%% @private
+-spec get_port_from_url(http_client:url(), binary()) -> integer() | undefined.
+get_port_from_url(URL, Hostname) ->
+    case binary:split(URL, Hostname) of
+        [_, <<":", PortAndPathPart/binary>>] ->
+            binary_to_integer(hd(string:split(PortAndPathPart, "/")));
+        _ ->
+            undefined
+    end.
---
+++ test/web_client/url_utils_test.erl
@@ -0,0 +1,104 @@
+%%%-------------------------------------------------------------------
+%%% @author Piotr Duleba
+%%% @copyright (C) 2021 ACK CYFRONET AGH
+%%% This software is released under the MIT license
+%%% cited in 'LICENSE.txt'.
+%%% @end
+%%%-------------------------------------------------------------------
+%%% @doc Test for functions defined in url_utils module.
+%%% @end
+%%%-------------------------------------------------------------------
+-module(url_utils_test).
+-author("Piotr Duleba").
+
+-ifdef(TEST).
+
+-include_lib("ctool/include/errors.hrl").
+-include_lib("ctool/include/test/test_utils.hrl").
+
+-define(URL_MAP(Scheme, Host, Port, Path, QueryString), #{
+    scheme => Scheme,
+    host => Host,
+    port => Port,
+    path => Path,
+    qs => QueryString
+}).
+
+-define(CORRECT_URL_BATCH, [
+    {<<"hostname:80">>, ?URL_MAP(http, <<"hostname">>, 80, <<"/">>, <<>>)},
+    {<<"hostname:80/path">>, ?URL_MAP(http, <<"hostname">>, 80, <<"/path">>, <<>>)},
+    {<<"hostname:443">>, ?URL_MAP(https, <<"hostname">>, 443, <<"/">>, <<>>)},
+    {<<"hostname:443/path">>, ?URL_MAP(https, <<"hostname">>, 443, <<"/path">>, <<>>)},
+
+    {<<"http://hostname">>, ?URL_MAP(http, <<"hostname">>, 80, <<"/">>, <<>>)},
+    {<<"http://hostname/path">>, ?URL_MAP(http, <<"hostname">>, 80, <<"/path">>, <<>>)},
+    {<<"https://hostname">>, ?URL_MAP(https, <<"hostname">>, 443, <<"/">>, <<>>)},
+    {<<"https://hostname/path">>, ?URL_MAP(https, <<"hostname">>, 443, <<"/path">>, <<>>)},
+
+    {<<"http://hostname:80">>, ?URL_MAP(http, <<"hostname">>, 80, <<"/">>, <<>>)},
+    {<<"http://hostname:80/path">>, ?URL_MAP(http, <<"hostname">>, 80, <<"/path">>, <<>>)},
+    {<<"https://hostname:443/">>, ?URL_MAP(https, <<"hostname">>, 443, <<"/">>, <<>>)},
+    {<<"https://hostname:443/path">>, ?URL_MAP(https, <<"hostname">>, 443, <<"/path">>, <<>>)},
+
+    {<<"http://hostname:443">>, ?URL_MAP(http, <<"hostname">>, 443, <<"/">>, <<>>)},
+    {<<"http://hostname:443/path">>, ?URL_MAP(http, <<"hostname">>, 443, <<"/path">>, <<>>)},
+    {<<"https://hostname:80">>, ?URL_MAP(https, <<"hostname">>, 80, <<"/">>, <<>>)},
+    {<<"https://hostname:80/path">>, ?URL_MAP(https, <<"hostname">>, 80, <<"/path">>, <<>>)},
+
+    {<<"http://hostname:1234">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<"http://hostname:1234/path">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+    {<<"https://hostname:1234">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<"https://hostname:1234/path">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+
+    {<<" http://hostname:1234">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<" http://hostname:1234/path">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+    {<<" https://hostname:1234">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<" https://hostname:1234/path">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+    {<<"http://hostname:1234  ">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<"http://hostname:1234/path  ">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+    {<<"https://hostname:1234  ">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<"https://hostname:1234/path  ">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+    {<<" http://hostname:1234  ">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<" http://hostname:1234/path  ">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+    {<<" https://hostname:1234  ">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<" https://hostname:1234/path  ">>, ?URL_MAP(https, <<"hostname">>, 1234, <<"/path">>, <<>>)},
+
+    %% TODO: VFS-7682 - move following cases to INCORRECT_URL_BATCH, as they should cause an error
+    {<<"hostname:1234">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/">>, <<>>)},
+    {<<"hostname:1234/path">>, ?URL_MAP(http, <<"hostname">>, 1234, <<"/path">>, <<>>)}
+]).
+
+-define(INCORRECT_URL_BATCH, [
+    <<"hostname">>,
+    <<"hostname/path">>,
+    <<"hostname:string">>,
+    <<"hostname:string/path">>,
+    <<"http://hostname:string">>,
+    <<"http://hostname:string/path">>,
+    <<"https://hostname:string">>,
+    <<"https://hostname:string/path">>,
+    <<"unknown://hostname">>,
+    <<"unknown://hostname/path">>,
+    <<"unknown://hostname:1234">>,
+    <<"unknown://hostname:1234/path">>
+]).
+
+
+correct_url_parse_test() ->
+    lists:foreach(fun({InputUrl, ExpResult}) ->
+        ?assertEqual(ExpResult, url_utils:infer_components(InputUrl))
+    end, ?CORRECT_URL_BATCH).
+
+
+incorrect_url_parse_test() ->
+    lists:foreach(fun(InputUrl) ->
+        ?assertThrow(?ERROR_MALFORMED_DATA, url_utils:infer_components(InputUrl))
+    end, ?INCORRECT_URL_BATCH).
+
+
+%%%===================================================================
+%%% Internal functions
+%%%===================================================================
+
+-endif.
+
